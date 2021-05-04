5eea87146aa43531b6c8be5a47955a4e1a78d308
NataliaCzachor14 <natalia.czachor@onet.pl>
2021-05-04 19:52:51 +0200

    Sprawdzenie testow

--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -44,7 +44,7 @@ describe('Users', () => {
 		var user = users.removeUser(userId);
 
 		expect(user.id).toBe(userId);
-		expect(users.users.length).toBe(2);
+		expect(users.users.length).toBe(5);
 	});
 
 	it('should NOT remove a user', () => {
@@ -59,7 +59,7 @@ describe('Users', () => {
 	it('should return names for Lakers Fans', () => {
 		var userList = users.getUserList('Lakers Fans');
 
-		expect(userList).toEqual(['Binh', 'Steph']);
+		expect(userList).toEqual(['A', 'Steph']);
 	});
 
 	it('should return names for Fad Diet', () => {
